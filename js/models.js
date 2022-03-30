2e2f71af94c11f8fcaf306c6b9b084a9ff34d4a2
Erick Rodrigues da Silva <53151822+programerSSS-cmd@users.noreply.github.com>
2022-03-30 12:14:19 -0300

    mudanças em todo progeto.

---
+++ js/models.js
@@ -0,0 +1,12 @@
+
+let modelsJson = [
+    {id:1, name:'Samsung Galaxy S20', img:'./img/example1.png', price:[2000.00, 2000.00, 2000.00], sizes:['1/72', '1/48', '1/32'], description:'Caça utilizado nos primeiros anos de combate no pacífico pela marinha dos EUA'},
+    {id:2, name:'Produto', img:'img/example2.png', price:[1300.00, 1680.00, 1550.50], sizes:['1/72', '1/48', '1/32'], description:'Caça de motor em estrela da Luftwaffe, temido em médias e baixas altitudes'},
+    {id:3, name:'Produto', img:'img/example3.png', price:[4000.00, 4500.00, 4150.00], sizes:['1/72', '1/48', '1/32'], description:'Caça fabricado pela Kawasaki para o exercito japones, com motor em linha'},
+    {id:4, name:'Produto', img:'img/example4.png', price:[2250.60, 2450.90, 2300.00], sizes:['1/72', '1/48', '1/32'], description:'Famoso caça da Messerchmitt, voado pelos maiores ases da Luftwaffe, a versão G foi a mais produzida'},
+    {id:5, name:'Produto', img:'img/example5.png', price:[2300.00, 2500.80, 2400.00], sizes:['1/72', '1/48', '1/32'], description:'Caça bombardeiro de fabricação americana utilizado pela FAB'},
+    {id:6, name:'Produto', img:'img/example6.png', price:[1200.00, 1550.00, 1300.00], sizes:['1/72', '1/48', '1/32'], description:'O mais conhecido caça americano, importante escolta para os bombardeiros diurnos'},
+    {id:7, name:'Produto', img:'img/example7.png', price:[5000.00, 5600.00, 5550.50], sizes:['1/72', '1/48', '1/32'], description:'O mais famosa caça britânico, utilizado em toda a guerra'},
+    {id:8, name:'Produto', img:'img/example8.png', price:[3500.00, 3650.00, 3700.00], sizes:['1/72', '1/48', '1/32'], description:'O mais famoso caça da marinha japonesa, terror do pacífico nos anos iniciais da guerra'},
+    {id:9, name:'Produto', img:'img/example9.png', price:[3800.00, 4200.00, 4000.00], sizes:['1/72', '1/48', '1/32'], description:'O mais famoso caça da marinha japonesa, terror do pacífico nos anos iniciais da guerra'}
+];
