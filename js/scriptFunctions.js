a4537f6c912914ff286493b9dcdd2b891c534bb2
Erick Rodrigues da Silva <53151822+programerSSS-cmd@users.noreply.github.com>
2022-03-30 16:56:35 -0300

    Alterações.
    Alterações sobre a responsividade de algumas telas, layout de alguns objetos e links das telas.

---
+++ js/scriptFunctions.js
@@ -0,0 +1,8 @@
+function salvarCad(){
+    alert("Cadastrado com sucesso!");
+    window.location.replace("Login.html");
+}
+function Finalizar(){
+    alert("Compra realizada com Sucesso!");
+    window.location.replace("Details.html");
+}
